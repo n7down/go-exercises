8b78ee634894abb9667ba75970c66c844289d900
Josh Adams <josh.adams@divvypay.com>
2021-07-08 15:09:45 -0600

    updated kata

--- NumberChars/internal/numberchars/numberchars.go
+++ NumberChars/internal/numberchars/numberchars.go
@@ -4,9 +4,11 @@ import "fmt"
 
 func NumberChars(input string) string {
 	var r string
-	var last string = ""
+    var first string = string(input[0])
+    var rest = string(input[1:])
+	var last string = first
 	var count int = 1
-	for _, c := range input {
+	for _, c := range rest {
 		if string(c) != last {
 			r += fmt.Sprintf("%v%v", count, string(c))
 			count = 1
