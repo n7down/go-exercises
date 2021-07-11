6083b0678cd159df5fe891673463581906db32df
Joshua Adams <n7down@gmail.com>
2021-07-11 15:01:13 -0700

    working on kata

--- Photos/internal/photos/photos.go
+++ Photos/internal/photos/photos.go
@@ -1,35 +1,79 @@
 package photos
 
-// you can also use imports, for example:
 import (
+	"fmt"
+	"sort"
 	"strings"
+	"time"
 )
 
-// import "os"
-
-// you can write to stdout for debugging purposes, e.g.
-// fmt.Println("this is a debug message")
+const (
+	layout = "2006-01-02 15:04:05"
+)
 
 type FileData struct {
-	Format string
-	Name   string
-	Date   string
-	Index  int
+	Extension string
+	Name      string
+	Timestamp time.Time
+}
+
+type FileDataIndexed struct {
+	Name      string
+	Index     int
+	Extension string
 }
 
 func RenamePhotos(s string) string {
-	// write your code in Go 1.4
-	var f []FileData
+	var (
+		fileDataList        []FileData
+		fileDataMap         = make(map[string]int)
+		fileDataIndexedList []FileDataIndexed
+		b                   strings.Builder
+	)
+
 	photos := strings.Split(s, "\n")
 	for _, photo := range photos {
 		data := strings.Split(photo, ",")
+		extension := strings.Split(strings.TrimSpace(data[0]), ".")
+		str := strings.TrimSpace(data[2])
+		timestamp, _ := time.Parse(layout, str)
 		fileData := FileData{
-			Format: strings.TrimSpace(data[0]),
-			Name:   strings.TrimSpace(data[1]),
-			Date:   strings.TrimSpace(data[2]),
+			Extension: extension[1],
+			Name:      strings.TrimSpace(data[1]),
+			Timestamp: timestamp,
 		}
-		f = append(f, fileData)
+		fileDataList = append(fileDataList, fileData)
 	}
 
-	return ""
+	// sort data by date
+	sort.Slice(fileDataList, func(i, j int) bool {
+		return fileDataList[i].Timestamp.Before(fileDataList[j].Timestamp)
+	})
+
+	// index the data
+	for _, f := range fileDataList {
+		var i int
+		if v, ok := fileDataMap[f.Name]; ok {
+			v = v + 1
+			fileDataMap[f.Name] = v
+			i = v
+		} else {
+			fileDataMap[f.Name] = 1
+			i = 1
+		}
+
+		fileDataIndexed := FileDataIndexed{
+			Name:      f.Name,
+			Index:     i,
+			Extension: f.Extension,
+		}
+
+		fileDataIndexedList = append(fileDataIndexedList, fileDataIndexed)
+	}
+
+	for _, f := range fileDataIndexedList {
+		b.WriteString(fmt.Sprintf("%s%d.%s\n", f.Name, f.Index, f.Extension))
+	}
+
+	return b.String()
 }
