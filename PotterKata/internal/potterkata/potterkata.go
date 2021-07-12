06893eb706bfff83e92211d781a1882fa7653e3d
Josh Adams <josh.adams@divvypay.com>
2021-07-12 16:09:50 -0600

    updated kata

--- PotterKata/internal/potterkata/potterkata.go
+++ PotterKata/internal/potterkata/potterkata.go
@@ -1,5 +1,9 @@
 package potterkata
 
+const (
+	bookPrice = 8.0
+)
+
 // TODO: potterkata - http://codingdojo.org/kata/Potter/
 func calculateDiscount(firstBook int, secondBook int, thirdBook int, fourthBook int, fifthBook int) float64 {
 	return 0.0
