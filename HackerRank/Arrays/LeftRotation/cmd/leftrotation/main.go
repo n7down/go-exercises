d0913b1af6653e07db4fd824260a76f85e0135b2
Joshua Adams <n7down@gmail.com>
2021-07-11 23:06:26 -0700

    added left rotation kata

---
+++ HackerRank/Arrays/LeftRotation/cmd/leftrotation/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/n7down/go-exercises/HackerRank/Arrays/LeftRotation/internal/leftrotation"
+)
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	nTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	dTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+	checkError(err)
+	d := int32(dTemp)
+
+	aTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var a []int32
+
+	for i := 0; i < int(n); i++ {
+		aItemTemp, err := strconv.ParseInt(aTemp[i], 10, 64)
+		checkError(err)
+		aItem := int32(aItemTemp)
+		a = append(a, aItem)
+	}
+
+	result := leftrotation.RotLeft(a, d)
+
+	for i, resultItem := range result {
+		fmt.Fprintf(writer, "%d", resultItem)
+
+		if i != len(result)-1 {
+			fmt.Fprintf(writer, " ")
+		}
+	}
+
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ HackerRank/Arrays/LeftRotation/internal/leftrotation/leftrotation.go
@@ -0,0 +1,15 @@
+package leftrotation
+
+/*
+ * Complete the 'rotLeft' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER d
+ */
+
+func RotLeft(a []int32, d int32) []int32 {
+	// Write your code here
+	return []int32{}
+}
