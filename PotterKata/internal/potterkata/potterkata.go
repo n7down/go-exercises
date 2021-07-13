739d27d861bd5ec6f26a83567aa922b91855d5be
Joshua Adams <n7down@gmail.com>
2021-07-12 22:24:47 -0700

    updating kata

--- PotterKata/internal/potterkata/potterkata.go
+++ PotterKata/internal/potterkata/potterkata.go
@@ -4,12 +4,55 @@ const (
 	bookPrice = 8.0
 )
 
-// TODO: potterkata - http://codingdojo.org/kata/Potter/
-func calculateDiscount(firstBookNum int, secondBookNum int, thirdBookNum int, fourthBookNum int, fifthBookNum int) float64 {
-	return 0.0
+func calculateDiscount(firstBookNum, secondBookNum, thirdBookNum, fourthBookNum, fifthBookNum int) float64 {
+	const (
+		twoBookDiscount   = 0.05
+		threeBookDiscount = 0.1
+	)
+
+	var (
+		bookCount int     = 0
+		discount  float64 = 0.0
+	)
+
+	if firstBookNum > 0 {
+		firstBookNum -= 1
+		bookCount += 1
+	}
+
+	if secondBookNum > 0 {
+		secondBookNum -= 1
+		bookCount += 1
+	}
+
+	if thirdBookNum > 0 {
+		thirdBookNum -= 1
+		bookCount += 1
+	}
+
+	if fourthBookNum > 0 {
+		fourthBookNum -= 1
+		bookCount += 1
+	}
+
+	if fifthBookNum > 0 {
+		fifthBookNum -= 1
+		bookCount += 1
+	}
+
+	if bookCount >= 3 {
+		// if contains 3 different books - (3 * bookPrice * threeBookDiscount)
+		discount = (3 * bookPrice * threeBookDiscount)
+
+	} else if bookCount == 2 {
+		// if contains 2 different books - (2 * bookPrice * twoBookDiscount)
+		discount = (2 * bookPrice * twoBookDiscount)
+	}
+
+	return discount
 }
 
-func CalculatePotterBookPrice(firstBookNum int, secondBookNum int, thirdBookNum int, fourthBookNum int, fifthBookNum int) float64 {
+func CalculatePotterBookPrice(firstBookNum, secondBookNum, thirdBookNum, fourthBookNum, fifthBookNum int) float64 {
 	var (
 		firstBookPrice  float64 = 0.0
 		secondBookPrice float64 = 0.0
