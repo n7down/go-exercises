6ec1559c5ccbba00a750fb2b9849e6b2d54c9099
Joshua Adams <n7down@gmail.com>
2021-07-13 22:50:16 -0700

    working on trip kata

---
+++ Trip/internal/trip/trip_test.go
@@ -0,0 +1,35 @@
+package trip
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTrip(t *testing.T) {
+	// var tests = []struct {
+	// 	input    int
+	// 	expected int
+	// }{
+	// 	{1, 2},
+	// }
+
+	// for _, test := range tests {
+	// 	actual := CalculatePotterBookPrice(test.firstBook, test.secondBook, test.thirdBook, test.fourthBook, test.fifthBook)
+	// 	assert.Equal(t, test.expected, actual, fmt.Sprintf("%f should return %f", actual, test.expected))
+	// }
+
+	// t := NewTrip()
+	// t.AddEdge("san franscico", "san diego")
+	// t.AddEdge("san diego", "las vegas")
+	// t.AddEdge("san diego", "los angeles")
+	// t.AddEdge("las vegas", "salt lake city")
+	// t.AddEdge("las vegas", "phoenix")
+	// t.AddEdge("phoenix", "san antonio")
+	// t.AddEdge("salt lake city", "denver")
+	// t.AddEdge("denver", "indianapolis")
+	// t.AddEdge("las vegas", "boise")
+	// t.AddEdge("boise", "alberta")
+
+	assert.Fail(t, "not implemented")
+}
